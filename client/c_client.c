4545d36f3c7eaa43ecd022bce60da4f3b46bec99
Armaan Agrawal <aa7385@nyu.edu>
2022-05-14 19:40:47 +0400

    k

--- client/c_client.c
+++ client/c_client.c
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
         bzero(&response,sizeof(response));
         
        
-        
+        bzero(&client_input,sizeof(client_input));
         printf("\nftp> ");
         fgets(command, sizeof(command), stdin); 
         
